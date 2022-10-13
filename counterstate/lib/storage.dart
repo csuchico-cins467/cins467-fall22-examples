3c01fad0c9b174087218d753eaf546ecb2c3abf9
Bryan Dixon <javawolfpack@gmail.com>
2022-10-13 10:20:29 -0700

    Flutter Storage part 2

--- counterstate/lib/storage.dart
+++ counterstate/lib/storage.dart
@@ -2,10 +2,13 @@
 
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:counterstate/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:uuid/uuid.dart';
 
@@ -22,6 +25,7 @@ class _SecondScreenState extends State<SecondScreen> {
   File? _image;
   bool _initialized = false;
   FirebaseApp? app;
+  Position? position;
 
   Future<void> initializeDefault() async {
     app = await Firebase.initializeApp(
@@ -88,13 +92,90 @@ class _SecondScreenState extends State<SecondScreen> {
       await initializeDefault();
     }
     if (_image != null) {
-      var uuid = const Uuid();
-      final String uid = uuid.v4();
-      if (kDebugMode) {
-        print(uid);
+      try {
+        position = await _determinePosition();
+        var uuid = const Uuid();
+        final String uid = uuid.v4();
+        if (kDebugMode) {
+          print(uid);
+        }
+        //upload
+        final String downloadURL = await _uploadFile(uid);
+        await _addItem(downloadURL, uid);
+        Navigator.pop(context);
+      } catch (e) {
+        if (kDebugMode) {
+          print(e.toString());
+        }
       }
-      //upload
-      Navigator.pop(context);
     }
+  }
+
+  Future<String> _uploadFile(String filename) async {
+    if (!_initialized) {
+      await initializeDefault();
+    }
+    final Reference ref = FirebaseStorage.instance.ref().child('$filename.jpg');
+    final SettableMetadata metadata =
+        SettableMetadata(contentType: 'image/jpeg', contentLanguage: 'en');
+    final UploadTask uploadTask = ref.putFile(_image!, metadata);
+    final String downloadURL = await (await uploadTask).ref.getDownloadURL();
+    if (kDebugMode) {
+      print(downloadURL);
+    }
+    return downloadURL;
+  }
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  Future<void> _addItem(String downloadURL, String title) async {
+    if (!_initialized) {
+      await initializeDefault();
+    }
+    await FirebaseFirestore.instance.collection("photos").add(<String, dynamic>{
+      'downloadURL': downloadURL,
+      'title': title,
+      'geopoint': GeoPoint(position!.latitude, position!.longitude),
+      'timestamp': DateTime.now()
+    });
   }
 }
